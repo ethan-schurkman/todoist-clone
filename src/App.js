e6d4e0b9234d0c82fe7120da97296754ed9899c2
ethan-schurkman <schurkman4@gmail.com>
2019-09-08 17:36:10 -0400

    add in context functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Header } from './components/layout/Header';
-import { Content } from './components/layout/Content'
+import { Content } from './components/layout/Content';
+import { ProjectsProvider, SelectedProjectProvider, ProjectsContext } from './context';
 
 export const App = () => (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+  <SelectedProjectProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+  </SelectedProjectProvider>
 );
